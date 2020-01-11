cc802242fed871702989a542fb1c1ffe84f750fa
Andrew Janssen <janssenandrew1@gmail.com>
2020-01-11 16:04:11 -0600

    fixed 2d

--- src/application/TwoDimensional.java
+++ src/application/TwoDimensional.java
@@ -8,11 +8,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 
 public class TwoDimensional implements Renderer {
   private int screenWidth;
@@ -47,45 +44,20 @@ public class TwoDimensional implements Renderer {
     return Math.min(x, y);
   }
 
-  public Scene buildScene2() {
-    GridPane grid = new GridPane();
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        Rectangle rect = new Rectangle();
-        rect.setWidth(scale);
-        rect.setHeight(scale);
-        rect.setFill(mapObject.getColor(i, j));
-        grid.add(rect, j, i);
-      }
-    }
-    StackPane stack = new StackPane();
-    Group group = new Group();
-    Line line = new Line(0, 0, 500, 500);
-    Line look = new Line(player.getPosition()[0] * scale, player.getPosition()[1] * scale,
-        (player.getPosition()[0] + player.getDirection()[0]) * scale,
-        (player.getPosition()[1] + player.getDirection()[1]) * scale);
-    line.setStroke(Color.TRANSPARENT);
-    look.setStroke(Color.YELLOW);
-    group.getChildren().addAll(line, look);
-    stack.getChildren().addAll(grid, group);
-    Scene scene = new Scene(stack, screenWidth, screenHeight);
-    return scene;
-  }
-
   public Scene buildScene() {
     for (int row = 0; row < height; row++) {
       for (int column = 0; column < map[row].length; column++) {
         buildSquare(row, column);
       }
     }
-    buildPlayer(player.getPosition(), player.getDirection());
+    Line line = buildPlayer(player.getPosition(), player.getDirection());
     WritableImage image = new WritableImage(screenWidth, screenHeight);
     PixelWriter pw = image.getPixelWriter();
     pw.setPixels(0, 0, screenWidth, screenHeight, PixelFormat.getIntArgbInstance(), pixels, 0,
         screenWidth);
     ImageView img = new ImageView();
     Group gr = new Group();
-    gr.getChildren().add(img);
+    gr.getChildren().addAll(img, line);
     img.setImage(image);
     return new Scene(gr, screenWidth, screenHeight);
   }
@@ -96,24 +68,21 @@ public class TwoDimensional implements Renderer {
     int startY = row * scale;
     int endX = (column + 1) * scale;
     int endY = (row + 1) * scale;
-    for (int x = startX; x < endX; x++) {
-      for (int y = startY; y < endY; y++) {
+    for (int x = startX; x < endX; x++)
+      for (int y = startY; y < endY; y++)
         pixels[y * screenWidth + x] =
             (255 & 0xff) << 24 | (((int) (255 * color.getRed())) & 0xff) << 16
                 | (((int) (255 * color.getGreen())) & 0xff) << 8
                 | (((int) (255 * color.getBlue())) & 0xff);
-      }
-    }
   }
 
-  private void buildPlayer(double[] position, double[] direction) {
+  private Line buildPlayer(double[] position, double[] direction) {
     int startX = (int) (position[0] * scale);
     int startY = (int) (position[1] * scale);
-    int endX = startX + (int) (direction[0] * scale);
-    int endY = startY + (int) (direction[1] * scale);
-    pixels[startY * screenWidth + startX] =
-        (255 & 0xff) << 24 | (96 & 0xff) << 16 | (96 & 0xff) << 8 | (96 & 0xff);
-    pixels[endY * screenWidth + endX] =
-        (255 & 0xff) << 24 | (96 & 0xff) << 16 | (96 & 0xff) << 8 | (96 & 0xff);
+    int endX = startX + (int) (direction[0] * (scale));
+    int endY = startY + (int) (direction[1] * (scale));
+    Line line = new Line(startX, startY, endX, endY);
+    line.setStroke(Color.YELLOW);
+    return line;
   }
 }
